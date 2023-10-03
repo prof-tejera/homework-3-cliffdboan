ae1ce46398afbf6f0ea4a2254cf41587422b24d7
Cliff Boan <cliffdboan@gmail.com>
2023-10-03 19:04:57 -0400

    Changes to LoginForm

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -1,7 +1,32 @@
+import Input from "components/Input/Input";
+import Button from "components/Button/Button";
 import "./LoginForm.css";
 
+const reqInput = (formType) => {
+  return <label className="req">{formType}: {Input({
+    label:formType,
+    class:"formInput",
+    placeholder:formType,
+    required: true,
+    type:formType.toLowerCase()
+  })}
+  </label>
+};
+
 const LoginForm = () => {
-  return null;
+
+  return (<div>
+    {reqInput("Username")}
+    <br/><br/>
+    {reqInput("Password")}
+    <br/><br/>
+    <input type="checkbox"></input>
+    <label> Remember Me?</label>
+    <br/><br/>
+    <Button
+    text="Submit"
+    />
+  </div>)
 };
 
 export default LoginForm;
--- src/Pager/Pager.js
+++ src/Pager/Pager.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import "./Pager.css";
 
 const Pager = () => {
-  return null;
+  const [index, totalPages] = useState(null);
+
+  // hilight current page
+  // left button indexes down one
+  // right button indexes up one
+  // selecting a specific number changes page to selected num
+  // have the page range from 1 to a var named totalPages
+  // // Have an input form that updates totalPages
+
+  // return (
+  // );
 };
 
 export default Pager;
